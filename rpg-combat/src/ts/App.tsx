ec3839b60a7419baa9b966811fca71390b945ccc
Danny <dannyrobinson94@hotmail.com>
2021-01-05 15:10:07 +0000

    Bootstrap

--- rpg-combat/src/ts/App.tsx
+++ rpg-combat/src/ts/App.tsx
@@ -4,21 +4,15 @@ import "../css/App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <h1 className="text-center m-5">Battle Simulator</h1>
+      <div className="container-fluid text-center">
+        <div className="row">
+          <div className="col-sm">One of three columns</div>
+          <div className="col-sm">One of three columns</div>
+          <div className="col-sm">One of three columns</div>
+        </div>
+      </div>
     </div>
   );
 }

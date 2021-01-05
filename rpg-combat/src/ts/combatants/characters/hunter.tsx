d71eeea3c1bf3749c82fe7db09fc3b774ae1c614
Danny <dannyrobinson94@hotmail.com>
2021-01-05 18:09:17 +0000

    Hunter health from reducer

--- rpg-combat/src/ts/combatants/characters/hunter.tsx
+++ rpg-combat/src/ts/combatants/characters/hunter.tsx
@@ -1,9 +1,21 @@
 import React from "react";
-import hunter from "../../../assets/hunter.jpg";
-import CharacterCard from "../character-card";
+import { connect, ConnectedProps } from "react-redux";
 
-const Hunter = () => (
-  <CharacterCard image={hunter} health={100} name="Hunter" />
+import hunter from "../../../assets/hunter.jpg";
+import { StoreState } from "../../redux-common/store";
+import CharacterCard from "../character-card";
+import { Characters } from "../contants";
+
+export type HunterProps = ConnectedProps<typeof connector>;
+
+const Hunter = ({ health }: HunterProps) => (
+  <CharacterCard image={hunter} health={health} name="Hunter" />
 );
 
-export default Hunter;
+export const mapState = (state: StoreState) => ({
+  health: state?.characterHealth[Characters.Hunter],
+});
+
+const connector = connect(mapState);
+
+export default connector(Hunter);
